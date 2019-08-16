01e935a2cd550776d0812eb776f13ace65d2dba2
Kai Reinhard <k.reinhard@micromata.de>
2019-08-16 06:57:59 +0200

    First steps of key cloak integration.

---
+++ projectforge-application/src/main/kotlin/org/projectforge/keycloak/KeyCloakConfiguration.kt
@@ -0,0 +1,92 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2019 Micromata GmbH, Germany (www.micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.keycloak
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+
+
+/**
+ * https://hub.docker.com/r/jboss/keycloak/
+ * https://www.baeldung.com/spring-boot-keycloak
+ *
+ * spring.profiles.active=keycloak
+ *
+ * keycloak.auth-server-url=http://localhost:9990/auth
+ * keycloak.realm=Micromata
+ * keycloak.resource=ProjectForge
+ * keycloak.public-client=true
+ * keycloak.principal-attribute=preferred_username
+ */
+@Configuration
+@Profile("keycloak")
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = [KeycloakSecurityComponents::class])
+open class KeyCloakConfiguration : KeycloakWebSecurityConfigurerAdapter() {
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(
+            auth: AuthenticationManagerBuilder) {
+
+        val keycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+                SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+    /*@Bean
+    fun KeycloakConfigResolver(): KeycloakSpringBootConfigResolver {
+        return KeycloakSpringBootConfigResolver()
+    }*/
+
+    @Bean
+    override protected fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(
+                SessionRegistryImpl())
+    }
+
+    @Throws(Exception::class)
+    override protected fun configure(http: HttpSecurity) {
+        super.configure(http)
+        http.authorizeRequests()
+                .antMatchers("/*")
+                .hasRole("user")
+                .anyRequest()
+                .permitAll()
+    }
+}
+
