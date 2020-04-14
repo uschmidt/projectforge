944bcb95ebc49252568cec6e0006f1af753f90e3
Jan Brümmer <j.bruemmer@micromata.de>
2020-04-14 12:07:16 +0200

    Add Plugin class

---
+++ plugins/org.projectforge.plugins.travel/src/main/java/org/projectforge/plugins/travel/TravelKostPlugin.java
@@ -0,0 +1,16 @@
+package org.projectforge.plugins.travel;
+
+import org.projectforge.plugins.core.AbstractPlugin;
+
+public class TravelKostPlugin extends AbstractPlugin {
+  public static final String RESOURCE_BUNDLE_NAME = "TravelI18nResources";
+
+  public TravelKostPlugin() {
+    super("travelkost", "Travel Kost", "");
+  }
+
+  @Override
+  protected void initialize() {
+
+  }
+}
