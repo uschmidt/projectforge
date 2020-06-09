e75f6b73f2759c46f9b8c0a8e1f4055ee84e342c
Jan Brümmer <j.bruemmer@micromata.de>
2020-06-09 15:19:08 +0200

    Bring Sql Files together + new path for sql files

--- plugins/org.projectforge.plugins.travel/src/main/resources/flyway/init/common/V1.0.1__Base_version.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE t_plugin_travel
-  ADD tenant_id INTEGER;
-
-CREATE INDEX idx_fk_t_plugin_travel_tenant_id
-  ON t_plugin_travel (tenant_id);
-
-ALTER TABLE t_plugin_travel
-  ADD CONSTRAINT fkt86thxrdu1i5r0f906a8 FOREIGN KEY (tenant_id) REFERENCES t_tenant (pk);
