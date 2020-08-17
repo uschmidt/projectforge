c610754b4803b8f673dcfb8a5b4e4d362954b43f
Jan Brümmer <j.bruemmer@micromata.de>
2020-08-17 15:35:18 +0200

    Add Employee Select

--- projectforge-rest/src/main/kotlin/org/projectforge/ui/AutoCompletion.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/ui/AutoCompletion.kt
@@ -47,7 +47,7 @@ class AutoCompletion<T>(
     /**
      * Pre-defined types of autocompletion objects as information for the clients.
      */
-    enum class Type { USER, GROUP, CUSTOMER, PROJECT, ADDRESS, TENANT, ACCOUNT, CALENDAR }
+    enum class Type { USER, GROUP, CUSTOMER, PROJECT, ADDRESS, TENANT, ACCOUNT, CALENDAR, EMPLOYEE }
     class Entry<T>(val value: T,
                    /**
                     * The title to display.
@@ -144,6 +144,13 @@ class AutoCompletion<T>(
          */
         fun getAutoCompletion4Calendars(): AutoCompletion<Int>? {
             return AutoCompletion(url = getAutoCompletionUrl("teamCal"), type = Type.CALENDAR.name)
+        }
+
+        /**
+         * @return category/autosearch?search=:search
+         */
+        fun getAutoCompletion4Employees(): AutoCompletion<Int>? {
+            return AutoCompletion(url = getAutoCompletionUrl("employee"), type = Type.EMPLOYEE.name)
         }
     }
 }
