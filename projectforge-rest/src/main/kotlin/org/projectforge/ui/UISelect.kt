6598d8e9386150d2f7ab283c9c53734a75716139
Jan Brümmer <j.bruemmer@micromata.de>
2020-08-24 10:42:01 +0200

    no message

--- projectforge-rest/src/main/kotlin/org/projectforge/ui/UISelect.kt
+++ projectforge-rest/src/main/kotlin/org/projectforge/ui/UISelect.kt
@@ -189,15 +189,5 @@ class UISelect<T>(val id: String,
                     autoCompletion = AutoCompletion.getAutoCompletion4Calendars(),
                     tooltip = tooltip ?: ElementsRegistry.getElementInfo(lc, id)?.tooltipI18nKey)
         }
-
-        fun createEmployeeSelect(lc: LayoutContext,id:String, multi: Boolean, label: String? = null, additionalLabel: String? = null, tooltip: String? = null): UISelect<Int> {
-            return UISelect<Int>(id,
-                    lc,
-                    multi = multi,
-                    label = label ?: ElementsRegistry.getElementInfo(lc, id)?.i18nKey,
-                    additionalLabel = additionalLabel ?: ElementsRegistry.getElementInfo(lc, id)?.additionalI18nKey,
-                    autoCompletion = AutoCompletion.getAutoCompletion4Employees(),
-                    tooltip = tooltip ?: ElementsRegistry.getElementInfo(lc, id)?.tooltipI18nKey)
-        }
     }
 }
