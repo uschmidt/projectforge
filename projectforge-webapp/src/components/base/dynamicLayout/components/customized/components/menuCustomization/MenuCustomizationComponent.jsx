091c6400149196303dcad99dbc5abf61ca2f6577
Kai Reinhard <k.reinhard@micromata.de>
2022-05-16 22:38:06 +0200

    WIP: customize my menu

--- projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/menuCustomization/MenuCustomizationComponent.jsx
+++ projectforge-webapp/src/components/base/dynamicLayout/components/customized/components/menuCustomization/MenuCustomizationComponent.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { closestCenter, DndContext, KeyboardSensor, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
 import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { MenuSortable } from './MenuSortable';
-// import { DynamicLayoutContext } from '../../../../context';
+import { DynamicLayoutContext } from '../../../../context';
 
 function MenuCustomizationComponent() {
-    // const { data, setData } = React.useContext(DynamicLayoutContext);
+    const { data, setData } = React.useContext(DynamicLayoutContext);
+    const { favoritesMenu } = data;
+    const { menuItems } = favoritesMenu;
+    console.log(menuItems, setData);
     const [items, setItems] = useState(['1', '2', '3']);
     const sensors = useSensors(
         useSensor(PointerSensor),
@@ -35,12 +38,14 @@ function MenuCustomizationComponent() {
             /* eslint-disable-next-line react/jsx-no-bind */
             onDragEnd={handleDragEnd}
         >
-            <SortableContext
-                items={items}
-                strategy={verticalListSortingStrategy}
-            >
-                {items.map((id) => <MenuSortable key={id} id={id} />)}
-            </SortableContext>
+            <ul>
+                <SortableContext
+                    items={menuItems}
+                    strategy={verticalListSortingStrategy}
+                >
+                    {menuItems.map((menuItem) => <MenuSortable key={menu} id={id} />)}
+                </SortableContext>
+            </ul>
         </DndContext>
     );
 }
